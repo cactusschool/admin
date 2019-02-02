7434029e5133021ed426b0fe520116e63f4eea41
ashismo <ashishkrmondal@gmail.com>
2019-02-02 03:17:30 -0500

    update

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ admin_process=`ps -ef | grep cactusschool-adminms | awk '$8=="java" {print $2}'`
 echo $admin_process
 kill -9 $admin_process
 echo "Running application has been stopped. Starting the application again"
-mvn -DskipTests=true clean package
+mvn -DskipTests=true clean install
 java -jar target/cactusschool-adminms-1.0.jar &
